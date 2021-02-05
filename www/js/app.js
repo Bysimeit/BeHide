bb31f0538031843b7f381e0ee9454da70bb7ebfa
Simon <35079000+Bysimeit@users.noreply.github.com>
2021-02-05 20:49:33 +0100

    Base

---
+++ www/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('deviceready', function () {
+    // App prête
+}, false);
+
+var app = angular.module('app', []);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/conversations', {templateUrl: 'pages/conversations.html'})
+        .when('/appels', {templateUrl: 'pages/appels.html'})
+        .when('/contacts', {templateUrl: 'pages/contacts.html'})
+        .otherwise({redirectTo: '/conversations'})
+})
