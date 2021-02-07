f0ffb85943d01e2687397067be353488796b0836
Teckinfor <Senechal.julien@outlook.com>
2021-02-07 16:37:05 +0100

    Issue

--- www/js/Menu.js
+++ www/js/Menu.js
@@ -5,5 +5,7 @@ function Menu($scope) {
 
 function changeBegin(){
     document.getElementById("menu").classList.remove("begin");
+    document.getElementById("profil").classList.remove("begin");
 }
+
 setTimeout(changeBegin,100);
