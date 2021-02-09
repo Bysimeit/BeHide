df91f9ff5b3daf2b63b4a3f1ae10e44019d5a68c
Teckinfor <Senechal.julien@outlook.com>
2021-02-09 12:58:44 +0100

    pas fini

---
+++ www/js/animationContact.js
@@ -0,0 +1,9 @@
+function stableContact(){
+    console.log("Contact");
+    document.getElementById("pageContact").classList.remove("hide-contact");
+    document.getElementById("lstcontact").classList.add("noscroll");
+    document.getElementById("pageContact").classList.add("apparition-contact");
+    document.getElementById("photocontact").classList.add("apparition-contact");
+    document.getElementById("pageContact").classList.add("apparition-contact");
+    photocontact
+}
